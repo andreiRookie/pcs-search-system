f2fbcc75a9ce9381f08722c7452b1b1c0ca7d39c
AndreiRookie <andrey.idem@gmail.com>
2023-06-14 20:56:56 +0400

    PageEntry compareTo implmntd

--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -3,5 +3,32 @@ public class PageEntry implements Comparable<PageEntry> {
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        if (this.count != o.count) {
+            return Integer.compare(this.count, o.count);
+        } else if (this.page != o.page) {
+            return Integer.compare(this.page, o.page);
+        } else {
+            return this.pdfName.compareTo(o.pdfName);
+        }
+    }
+
+    public String getPdfName() {
+        return pdfName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
