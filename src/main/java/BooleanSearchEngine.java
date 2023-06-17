6f2b095815d271e80402ab358dca7e2e287bb4a1
AndreiRookie <andrey.idem@gmail.com>
2023-06-17 15:40:46 +0400

    minor fixes; getting files w DirectoryStream

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,11 +1,14 @@
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class BooleanSearchEngine implements SearchEngine {
 
@@ -13,12 +16,7 @@ public class BooleanSearchEngine implements SearchEngine {
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
 
-        List<File> pdfFiles = new ArrayList<>();
-        if (pdfsDir.exists() && pdfsDir.isDirectory()) {
-            pdfFiles = Stream.of(pdfsDir.listFiles())
-                    .filter(file -> !file.isDirectory())
-                    .collect(Collectors.toList());
-        }
+        List<File> pdfFiles = getPdfFiles(pdfsDir);
 
         for (File file : pdfFiles) {
             var doc = new PdfDocument(new PdfReader(file));
@@ -26,20 +24,9 @@ public class BooleanSearchEngine implements SearchEngine {
             for (var i = 1; i <= doc.getNumberOfPages(); i++) {
                 var page = doc.getPage(i);
 
-                var text = PdfTextExtractor.getTextFromPage(page);
-                var words = text.split("\\P{IsAlphabetic}+");
+                Map<String, Integer> currPageWordsFreq = getPageWordsFreq(page);
 
-                Map<String, Integer> currPageWordFreqs = new HashMap<>();
-
-                for (var word : words) {
-                    if (word.isEmpty()) {
-                        continue;
-                    }
-                    word = word.toLowerCase();
-                    currPageWordFreqs.put(word, currPageWordFreqs.getOrDefault(word, 0) + 1);
-                }
-
-                for (Map.Entry<String, Integer> entry : currPageWordFreqs.entrySet()) {
+                for (Map.Entry<String, Integer> entry : currPageWordsFreq.entrySet()) {
 
                     var pageEntry = new PageEntry(file.getName(), i, entry.getValue());
 
@@ -57,6 +44,37 @@ public class BooleanSearchEngine implements SearchEngine {
     public List<PageEntry> search(String word) {
         return wordPageEntries.getOrDefault(word, Collections.emptyList());
 
+    }
+
+    private List<File> getPdfFiles(File pdfsDir) throws IOException {
+        List<File> pdfFiles = new ArrayList<>();
+
+        if (pdfsDir.exists() && pdfsDir.isDirectory()) {
+            try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(Paths.get(pdfsDir.getAbsolutePath()))) {
+                for (Path path : dirStream) {
+                    if (!Files.isDirectory((path))) {
+                        pdfFiles.add(path.toFile());
+                    }
+                }
+            }
+        }
+        return pdfFiles;
+    }
+
+    private Map<String, Integer> getPageWordsFreq(PdfPage page) {
+        var text = PdfTextExtractor.getTextFromPage(page);
+        var words = text.split("\\P{IsAlphabetic}+");
+
+        Map<String, Integer> wordsFreq = new HashMap<>();
+
+        for (var word : words) {
+            if (word.isEmpty()) {
+                continue;
+            }
+            word = word.toLowerCase();
+            wordsFreq.put(word, wordsFreq.getOrDefault(word, 0) + 1);
+        }
+        return wordsFreq;
     }
 
     public Map<String, List<PageEntry>> getWordPageEntries() {
